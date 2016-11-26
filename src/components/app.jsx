4409357da6f2ea0dedd252b6b2a563682a73cea0
Kyle Williams-Smith <kyle.i.williamssmith@gmail.com>
2016-11-26 15:43:28 -0800

    Empty project for personal website. Empty package json, no redirection to resume

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+   
+   render() {
+      return (
+         <div className="red">
+            <h1>KDAWG 2016 YO</h1>
+         </div>
+      );
+   }
+}
--- src/default.js
+++ src/default.js
@@ -1,5 +1,9 @@
+import React from "react";
+import {render} from "react-dom";
+import App from "./components/app.jsx";
+
 // Kyle Williams-Smith
 // Default.js
 // Contains default behavior for the webpage.
 
-window.location = "src/pdfs/resume.pdf";
+render(<App/>, document.getElementById("id"));
